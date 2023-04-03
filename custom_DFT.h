47e629fd08ac5169807e4cd921396935c0d5c3c8
Vestnikov_Roman <vestnikov.rn@edu.spbstu.ru>
2023-04-03 15:40:57 +0300

    удаление мусора

--- custom_DFT.h
+++
@@ -1,8 +0,0 @@
-#include "opencv2/core.hpp"
-#include "opencv2/highgui.hpp"
-#include <opencv2/imgproc.hpp>
-#include <iostream>
-
-cv::Mat DFT_lobovoy(const cv::Mat& x_, const cv::Mat &W);
-cv::Mat get_W(int N);
-cv::Mat get_W_inv(int N);
